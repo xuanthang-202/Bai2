419ba1eaee49093a5eed8b4d04dc686df74f7c6a
xuanthang-202 <thangcaovct@gmail.com>
2025-10-07 16:34:46 +0700

    Bai2_1.py

--- Bai2_1.py
+++ Bai2_1.py
@@ -1,3 +1,7 @@
+print("Sinh vien: Cao Xuan Thang")
+print("Ma so SV : 245752021610145")
+print("###########################")
+#################################
 n1 = int(input("Enter n1 value: "))
 n2 = int(input("Enter n2 value: "))
 
@@ -5,3 +9,4 @@ if n1 > n2:
     print("n1 is big")
 else:
     print("n2 is big")
+
